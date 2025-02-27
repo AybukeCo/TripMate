800e7d8cc2643f51d5018d5c776fb51cb5c4a320
Tearai <77899947+Tearai@users.noreply.github.com>
2025-02-27 16:13:45 +0000

    Chatting system template added
    Added the template of the chatting page. Need to add js function to send message into screen. (No back-end yet).

---
+++ tripmate 2_22_25/chatsystem.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".go-back-page").addEventListener("click", function() {
+        window.location.href = "chat.html";
+      });
+});
